e224ef0072e52eacd1fd4be024625a1fecdc581e
John Wahlstedt <johnwahlstedt@Johns-MacBook-Pro-2.local>
2020-10-19 22:41:15 -0500

    Changed the county map to different format, i can add data next and get the slider and check boxes working.

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ import plotly.figure_factory as ff
 import numpy as np
 
 import matplotlib.pyplot as plt
+import pathlib
+import os
 
 # Load data
 
@@ -31,7 +33,14 @@ df2 = pd.read_csv(
     'appdata2.csv')
 
 
-df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/laucnty16.csv')
+# Load data
+
+
+# df_lat_lon = pd.read_csv("CountyData.csv", encoding='cp1252')
+
+# Load data
+
+df_sample = pd.read_csv('Unemployment2016.csv')
 df_sample['State FIPS Code'] = df_sample['State FIPS Code'].apply(lambda x: str(x).zfill(2))
 df_sample['County FIPS Code'] = df_sample['County FIPS Code'].apply(lambda x: str(x).zfill(3))
 df_sample['FIPS'] = df_sample['State FIPS Code'] + df_sample['County FIPS Code']
@@ -49,7 +58,7 @@ app = dash.Dash(
 server = app.server
 
 
-DEFAULT_COLORSCALE = [
+colorscale = [
     "#f2fffb",
     "#bbffeb",
     "#98ffe0",
@@ -148,7 +157,7 @@ app.layout = html.Div(
                                 {'label': 'Population', 'value': 'POP'},
                                 {'label': 'Unemployment', 'value': 'Unemployment'},
                             ],
-                            value=['GDP', 'POP', 'Unemployment']),
+                            value=['Null', 'Null', 'Unemployment']),
                         html.Div(
                             id="heatmap-container",
                             children=[
@@ -221,96 +230,6 @@ app.layout = html.Div(
         ),
         ])
 
-# app.layout = html.Div(children=[
-#                         html.Div(
-#                             id="root",
-#                             children=[
-#                                 html.Div(
-#                                     id="header",
-#                                     children=[
-#                                         html.H1(children="3D Printing Community Python App"),
-#                                         html.P(
-#                                             id="description",
-#                                             children="Interactive app for data visualization",
-#                                                 ),
-#                                     ], style={"border":"3px black solid"})
-#                                     ]),
-#
-#
-#              html.Div(className='row',  # Define the row element
-#                       children=[
-#                       html.Div(className='three columns div-user-controls',
-#                                     children=[
-#                                        html.H2('United States Geographic Map:'),
-#                                        html.Div(className='div-for-dropdown',
-#                                             children=[
-#                                                 dcc.Dropdown(id='Map Selection',
-#                                                    options=get_map_options(map_vals),
-#                                                    multi=False,
-#                                                    value=[map_vals[0]],
-#                                                    style={'backgroundColor': '#1E1E1E'},
-#                                                    className='MapSelector')
-#                                                   ],
-#                                                     style={'color': '#1E1E1E'}),
-#
-#                                        html.H2('ScatterPlot:'),
-#                                        html.H2('X-Axis for Scatterplot'),
-#                                        html.Div(className='X-Axis',
-#                                              children=[
-#                                                  dcc.Dropdown(id='X-Axis Select',
-#                                                     options=get_xax_options(x_axis),
-#                                                     multi=False,
-#                                                     value=[x_axis[0]],
-#                                                     style={'backgroundColor': '#1E1E1E'},
-#                                                     className='ScatterSelector')
-#                                                    ],
-#                                                      style={'color': '#1E1E1E'}),
-#
-#                                        html.H2('Y-Axis for Scatterplot'),
-#                                        html.Div(className='Y-Axis',
-#                                              children=[
-#                                                  dcc.Dropdown(id='Y-Axis Select',
-#                                                     options=get_yax_options(y_axis),
-#                                                     multi=False,
-#                                                     value=[y_axis[0]],
-#                                                     style={'backgroundColor': '#1E1E1E'},
-#                                                     className='ScatterSelector')
-#                                                    ],
-#                                                      style={'color': '#1E1E1E'})
-#                                                      ],
-#                                                      ),
-#
-#                         html.Div(id="app-container",
-#                                         children=[
-#                                             html.Div(className='five columns div-for-charts bg-grey',
-#                                                         children=[
-#                                                             html.Div(id="slider-container",
-#                                                                 children=[
-#                                                                     html.P(
-#                                                                         id="slider-text",
-#                                                                         children="Drag the slider to change the year:",
-#                                                                             ),
-#                                                                     dcc.Slider(
-#                                                                 id="years-slider",
-#                                                                 min=min(YEARS),
-#                                                                 max=max(YEARS),
-#                                                                 value=min(YEARS),
-#                                                                 marks={
-#                                                                     str(year): {
-#                                                                         "label": str(year),
-#                                                                         "style": {"color": "#7fafdf"},
-#                                                                     }
-#                                                                     for year in YEARS
-#                                                                 },
-#                                                             ),
-#                                                         ],
-#                                                     ),
-#                                                             dcc.Graph(id = "Map"),
-#                                                             dcc.Graph(id='Scatterplot',config={'displayModeBar': False},animate=False)] ,
-#                                                             style={"border":"3px black solid"}),
-#                                                     ])
-#                                                 ])
-#                                      ])
 
 ###################################
 
@@ -322,145 +241,27 @@ app.layout = html.Div(
 )
 def display_map(year, figure):
 
-
-    colorscale = ["#f2fffb",
-    "#bbffeb",
-    "#98ffe0",
-    "#79ffd6",
-    "#6df0c8",
-    "#69e7c0",
-    "#59dab2",
-    "#45d0a5",
-    "#31c194",
-    "#2bb489",
-    "#25a27b",
-    "#1e906d",
-    "#188463",
-    "#157658",
-    "#11684d",
-    "#10523e",
-]
-
     endpts = list(np.linspace(1, 12, len(colorscale) - 1))
     fips = df_sample['FIPS'].tolist()
     values = df_sample['Unemployment Rate (%)'].tolist()
 
+
     fig = ff.create_choropleth(
-        fips=fips, values=values,
-        binning_endpoints=endpts,
-        colorscale=colorscale,
+        fips=fips, values=values, scope=['usa'],
+        binning_endpoints=endpts, colorscale=colorscale,
         show_state_data=False,
-        show_hover=True, centroid_marker={'opacity': 0},
-        asp=2.9,
-        legend_title='% unemployed',
-        paper_bgcolor='#1f2630',
-        plot_bgcolor='#1f2630')
+        show_hover=True,
+        asp = 2.9,
+        legend_title = '% unemployed'
+    )
 
 
+    fig.layout.paper_bgcolor="#252b33"
+    fig.layout.plot_bgcolor="#252b33"
 
     return fig
 
 
-
-# # Callback for Map
-# @app.callback(Output('county-choropleth', 'figure'),
-#               [Input('county-choropleth', 'value')])
-# def update_map(selected_dropdown_value):
-#
-#         mapval = df['2019']
-#
-#         if selected_dropdown_value == '2015 GDP per capita':
-#           mapval = df['2015']
-#
-#         elif selected_dropdown_value == '2016 GDP per capita':
-#           mapval = df['2016']
-#
-#         elif selected_dropdown_value == '2017 GDP per capita':
-#           mapval = df['2017']
-#
-#         elif selected_dropdown_value == '2018 GDP per capita':
-#           mapval = df['2018']
-#
-#         elif selected_dropdown_value == '2019 GDP per capita':
-#           mapval = df['2019']
-#
-#         elif selected_dropdown_value == '2020 Population':
-#           mapval = df['Pop2020']
-#
-#         elif selected_dropdown_value == '2018 Population':
-#           mapval = df['Pop2018']
-#
-#         elif selected_dropdown_value == 'Log 2020 Population':
-#           mapval = df['Log Pop 2020']
-#
-#         elif selected_dropdown_value == 'Log 2018 Population':
-#           mapval = df['Log Pop 2018']
-#
-#         elif selected_dropdown_value == 'Number of Universites Per State':
-#             mapval = df['Number of Universites Per State']
-#
-#         elif selected_dropdown_value == 'Number of Universites Per State (No CA)':
-#             mapval = df['Number of Universites Per State (No CA)']
-#
-#         # figure = go.Figure(
-#         #     data=go.Choropleth(
-#         #     locations=df['GeoName'], # Spatial coordinates
-#         #     z = mapval, # Data to be color-coded
-#         #     locationmode = 'USA-states', # set of locations match entries in `locations`
-#         #     colorscale = 'Reds',
-#         #     colorbar_title = "Density"),
-#         #     layout = go.Layout(geo=dict(bgcolor= 'rgba(0,0,0,0)', lakecolor='#1E1E1E',
-#         #       landcolor='rgba(51,17,0,0.2)',
-#         #       subunitcolor='black'),
-#         #       title = 'Geographic Data: ' + str( selected_dropdown_value ),
-#         #       font = {"size": 9, "color":"White"},
-#         #       titlefont = {"size": 15, "color":"White"},
-#         #       geo_scope='usa',
-#         #       margin={"r":0,"t":40,"l":0,"b":0},
-#         #       paper_bgcolor='#1E1E1E',
-#         #       plot_bgcolor='#1E1E1E',
-#         #       )
-#         #       )
-#         # return figure
-#
-#         figure = dff.iplot(
-#                 kind="area",
-#                 x="Year",
-#                 y="Age Adjusted Rate",
-#                 text="County",
-#                 categories="County",
-#                 colors=[
-#                     "#1b9e77",
-#                     "#d95f02",
-#                     "#7570b3",
-#                     "#e7298a",
-#                     "#66a61e",
-#                     "#e6ab02",
-#                     "#a6761d",
-#                     "#666666",
-#                     "#1b9e77",
-#                 ],
-#                 vline=[year],
-#                 asFigure=True,
-#             )
-#
-#         fig_layout["yaxis"]["title"] = "Age-adjusted death rate per county per year"
-#         fig_layout["xaxis"]["title"] = ""
-#         fig_layout["yaxis"]["fixedrange"] = True
-#         fig_layout["xaxis"]["fixedrange"] = False
-#         fig_layout["hovermode"] = "closest"
-#         fig_layout["title"] = "<b>{0}</b> counties selected".format(len(fips))
-#         fig_layout["legend"] = dict(orientation="v")
-#         fig_layout["autosize"] = True
-#         fig_layout["paper_bgcolor"] = "#1f2630"
-#         fig_layout["plot_bgcolor"] = "#1f2630"
-#         fig_layout["font"]["color"] = "#2cfec1"
-#         fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
-#         fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
-#         fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
-#         fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
-#
-#         return figure
 
 
 # Callback for Map
@@ -539,11 +340,13 @@ def update_scatter(x1,y1):
            trendline = 'ols'))
 
 
-    figure.layout.paper_bgcolor="#1f2630"
-    figure.layout.plot_bgcolor="#1f2630"
+
+    figure.layout.paper_bgcolor="#252b33"
+    figure.layout.plot_bgcolor="#252b33"
 
 
     return figure
+
 
 
 if __name__ == "__main__":
